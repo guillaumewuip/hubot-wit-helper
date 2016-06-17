622901da89aa8edc6b58bd6a41307eafe9d8126a
Guillaume Wuip <guillaume@wuips.com>
2016-06-17 07:56:17 +0200

    Make lib package

---
+++ index.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+(() => {
+
+    module.exports = {
+        SessionWrapper:     require('./lib/SessionWrapper'),
+        ContextManager:     require('./lib/ContextManager'),
+        firstEntityValue:   require('./lib/firstEntityValue'),
+        Robot:              require('./lib/Robot'),
+    };
+
+})();
