acf485eeabc75e418cafdfd13b7a22f25b459bf9
Guillaume Wuip <guillaume@wuips.com>
2016-07-07 08:04:07 +0200

    The problem is not the require

--- index.js
+++ index.js
@@ -3,21 +3,11 @@
 
 (() => {
 
-    const path = require('path');
-
     module.exports = {
-        SessionWrapper: require(
-            path.join(__dirname, './lib/SessionWrapper')
-        ),
-        ContextManager: require(
-            path.join(__dirname, './lib/ContextManager')
-        ),
-        firstEntityValue: require(
-            path.join(__dirname, './lib/firstEntityValue')
-        ),
-        Robot: require(
-            path.join(__dirname, './lib/Robot')
-        ),
+        SessionWrapper:     require('./lib/SessionWrapper'),
+        ContextManager:     require('./lib/ContextManager'),
+        firstEntityValue:   require('./lib/firstEntityValue'),
+        Robot:              require('./lib/Robot'),
     };
 
 })();
